6ec99bbf0112c511331cb8f5b30c776311e2e446
RN Adilla <16340012@student.ummetro.ac.id>
2019-08-21 14:36:51 +0700

    Laravel Twilio

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+ 
+Route::get('/', 'HomeController@show');
+Route::post('/', 'HomeController@storePhoneNumber');
+Route::post('/custom', 'HomeController@sendCustomMessage');
